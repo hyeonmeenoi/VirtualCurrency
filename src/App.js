d773a01a7a201a4004b342c9e8b4715a5a411c84
Hyeonmin Kwon <hyeonmeenoi@gmail.com>
2023-07-20 00:13:26 +0900

    9. Setting Basename

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Main />} />
+        <Route
+          basename={process.env.PUBLIC_URL}
+          path={`${process.env.PUBLIC_URL}/`}
+          element={<Main />}
+        />
         <Route
           path={`${process.env.PUBLIC_URL}/coin/:id`}
+          basename={process.env.PUBLIC_URL}
           element={<Detail />}
         />
       </Routes>
